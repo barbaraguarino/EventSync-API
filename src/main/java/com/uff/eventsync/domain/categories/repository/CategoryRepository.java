74257d6395fa68f38479ddf4b12d1e9e0cd083ac
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-06-12 18:39:04 -0300

    Atualização do repositório

--- src/main/java/com/uff/eventsync/domain/categories/repository/CategoryRepository.java
+++ src/main/java/com/uff/eventsync/domain/categories/repository/CategoryRepository.java
@@ -5,7 +5,10 @@ import com.uff.eventsync.domain.categories.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, UUID> {}
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+    List<Category> findAllByOrderByNameAsc();
+}
