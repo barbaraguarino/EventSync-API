8e843fe468f888d28845d9bad99e7290ae8ece49
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-06-12 18:32:16 -0300

    Adição dos dados para popular o banco de dados de categorias

--- src/main/resources/db/migration/V2__create-table-categories.sql
+++ src/main/resources/db/migration/V2__create-table-categories.sql
@@ -1,4 +1,18 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
 CREATE TABLE categories (
     id UUID PRIMARY KEY,
     name VARCHAR(100) NOT NULL UNIQUE,
 );
+
+INSERT INTO categories (id, name) VALUES
+    (gen_random_uuid(), 'Música'),
+    (gen_random_uuid(), 'Tecnologia'),
+    (gen_random_uuid(), 'Esportes'),
+    (gen_random_uuid(), 'Arte & Cultura'),
+    (gen_random_uuid(), 'Comida & Bebida'),
+    (gen_random_uuid(), 'Negócios & Networking'),
+    (gen_random_uuid(), 'Saúde & Bem-estar'),
+    (gen_random_uuid(), 'Educação'),
+    (gen_random_uuid(), 'Games'),
+    (gen_random_uuid(), 'Festas & Vida Noturna');
