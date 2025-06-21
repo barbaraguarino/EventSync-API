c1cf1429a179dfff5a9f0f55e214c6a1e6cafd34
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-06-21 20:36:01 -0300

    Update EventController.java

--- src/main/java/com/uff/eventsync/presentation/event/EventController.java
+++ src/main/java/com/uff/eventsync/presentation/event/EventController.java
@@ -3,16 +3,22 @@ package com.uff.eventsync.presentation.event;
 import com.uff.eventsync.application.event.dto.EventCreateRequestDTO;
 import com.uff.eventsync.application.event.dto.EventDetailResponseDTO;
 import com.uff.eventsync.application.event.dto.EventResponseDTO;
+import com.uff.eventsync.application.event.dto.EventSummaryResponseDTO;
+import com.uff.eventsync.application.event.mapper.EventMapper;
 import com.uff.eventsync.application.event.service.EventService;
+import com.uff.eventsync.domain.event.entity.Event;
 import com.uff.eventsync.domain.user.entity.User;
 import jakarta.validation.Valid;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -53,4 +59,18 @@ public class EventController {
 
         return ResponseEntity.ok(event);
     }
+
+    @GetMapping
+    public ResponseEntity<CollectionModel<EventSummaryResponseDTO>> getAllEvents() {
+        List<Event> events = eventService.findAllEventsSorted();
+        List<EventSummaryResponseDTO> eventDTOs = events.stream().map(event -> {
+            EventSummaryResponseDTO dto = EventMapper.toSummaryResponseDTO(event);
+            dto.add(linkTo(methodOn(EventController.class).getEventById(event.getId())).withSelfRel());
+            return dto;
+        }).collect(Collectors.toList());
+        var selfLink = linkTo(methodOn(EventController.class).getAllEvents()).withSelfRel();
+        CollectionModel<EventSummaryResponseDTO> collectionModel = CollectionModel.of(eventDTOs, selfLink);
+        return ResponseEntity.ok(collectionModel);
+    }
+
 }
