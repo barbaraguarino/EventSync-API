ad3c722bbe9c03e3eb87cde9fa42347a3fc24944
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-06-21 20:35:39 -0300

    Update EventRepository.java

--- src/main/java/com/uff/eventsync/domain/event/repository/EventRepository.java
+++ src/main/java/com/uff/eventsync/domain/event/repository/EventRepository.java
@@ -4,9 +4,11 @@ import com.uff.eventsync.domain.event.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, UUID> {
+    List<Event> findAllByOrderByDateAscStartTimeAsc();
 
 }
