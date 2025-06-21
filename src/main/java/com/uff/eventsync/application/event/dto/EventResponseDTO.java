cf0e660ab243eb3ab5273f3d07f7aab13074c7ed
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-06-21 20:59:13 -0300

    Correções

--- src/main/java/com/uff/eventsync/application/event/dto/EventResponseDTO.java
+++ src/main/java/com/uff/eventsync/application/event/dto/EventResponseDTO.java
@@ -1,9 +1,21 @@
 package com.uff.eventsync.application.event.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record EventResponseDTO(
-        UUID id,
-        String name
-) {}
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventResponseDTO extends RepresentationModel<EventResponseDTO> {
+    private UUID id;
+    private String name;
+    private LocalDate date;
+    private String categoryName;
+}
