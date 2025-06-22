f9f3672f13cfb42cb03da05ff52ed8a74abd5840
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-06-22 20:33:38 -0300

    Service

--- src/main/java/com/uff/eventsync/application/registration/service/RegistrationService.java
+++ src/main/java/com/uff/eventsync/application/registration/service/RegistrationService.java
@@ -1,9 +1,12 @@
 package com.uff.eventsync.application.registration.service;
 
+import com.uff.eventsync.domain.event.entity.Event;
 import com.uff.eventsync.domain.user.entity.User;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface RegistrationService {
     void checkInToEvent(UUID eventId, User currentUser);
+    List<Event> findAttendedEventsForUser(User currentUser);
 }
