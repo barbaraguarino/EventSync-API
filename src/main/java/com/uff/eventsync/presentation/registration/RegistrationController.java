b9fc3f26238d1f13cb2e253b13feea62d1b52d97
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-06-22 20:20:07 -0300

    Create RegistrationController.java

---
+++ src/main/java/com/uff/eventsync/presentation/registration/RegistrationController.java
@@ -0,0 +1,30 @@
+package com.uff.eventsync.presentation.registration;
+
+import com.uff.eventsync.application.registration.service.RegistrationService;
+import com.uff.eventsync.domain.user.entity.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/events/{eventId}/checkin")
+public class RegistrationController {
+
+    private final RegistrationService registrationService;
+
+    public RegistrationController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> checkIn(@PathVariable UUID eventId, Authentication authentication) {
+        User currentUser = (User) authentication.getPrincipal();
+        registrationService.checkInToEvent(eventId, currentUser);
+        return ResponseEntity.noContent().build();
+    }
+}
