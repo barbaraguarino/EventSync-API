21db93fcf8a3540b21e8302cd25003df6de78f84
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-06-26 17:36:58 -0300

    Update RegistrationController.java

--- src/main/java/com/uff/eventsync/presentation/registration/RegistrationController.java
+++ src/main/java/com/uff/eventsync/presentation/registration/RegistrationController.java
@@ -4,10 +4,7 @@ import com.uff.eventsync.application.registration.service.RegistrationService;
 import com.uff.eventsync.domain.user.entity.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -25,6 +22,13 @@ public class RegistrationController {
     public ResponseEntity<Void> checkIn(@PathVariable UUID eventId, Authentication authentication) {
         User currentUser = (User) authentication.getPrincipal();
         registrationService.checkInToEvent(eventId, currentUser);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteCheckIn(@PathVariable UUID eventId, Authentication authentication) {
+        User currentUser = (User) authentication.getPrincipal();
+        registrationService.deleteCheckInToEvent(eventId, currentUser);
         return ResponseEntity.noContent().build();
     }
 }
